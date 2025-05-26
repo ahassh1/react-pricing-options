d1e33eeb64af3fcedd6d4187e8d283f097e9e25a
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-05-26 20:17:10 +0600

    explore axios js and process data for the chart

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,32 @@ import DaisyNav from "./components/DaisyNav/DaisyNav";
 import Navbar from "./components/Navbar/Navbar";
 import PricingOptions from "./components/PricingOptions/PricingOptions";
 import ResultsChart from "./components/ResultsChart/ResultsChart";
+import axios from "axios";
+import MarkCharts from "./components/MarkCharts/MarkCharts";
 
 const pricingPromise = fetch('pricingData.json').then(res=>res.json());
+
+const marksPromise = axios.get('marksData.json')
 
 function App() {
   return (
     <>
- 
+     
       <Navbar></Navbar>
       {/* <DaisyNav></DaisyNav> */}
-
+            
       <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
-
+         
         <PricingOptions pricingPromise={pricingPromise}></PricingOptions>
       </Suspense>
       <ResultsChart></ResultsChart>
+      
+
+        <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
+          <MarkCharts marksPromise={marksPromise}></MarkCharts>
+        </Suspense>
+      
+      
    
     </>
   );
